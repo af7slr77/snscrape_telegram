8c5311391096e4c958280dc0dfa0c121481b85fe
af7slr77 <fedorovas722@gmail.com>
2023-01-13 07:07:37 -0500

    test commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from snscrape.modules.telegram import TelegramChannelScraper, TelegramPost
+scraper = TelegramChannelScraper('klimzhukoff')  # создаем экземпляр класса, твитера, тг, вк,  и т.д, передаем
+# назавание канала
+
+result = []
+
+
+for i, item in enumerate(scraper.get_items()):  # с помощью  get_items получаем все посты с канала (аккаунта)
+        result.append(item)
+        #print(i, item.content)  # с помощью  content, date получаем все записи из поста\ дату и время
+        scr = TelegramPost(url=item.url, date=item.date, outlinks=item.outlinks, outlinksss=item.outlinksss, content=item.content)
+        print(scr.content)
+        if i == 10:
+
+            break
+######################## подумать как спарсить каналы по запросу через поиск
